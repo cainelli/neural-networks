54b7aac6a7f4996c62af87c9497eb93a679fe6c0
Fernando Cainelli <fernando.cainelli@gmail.com>
2016-09-28 23:58:08 -0300

    WIP: one step further

--- normaliz/components/parser.py
+++ normaliz/components/parser.py
@@ -2,7 +2,9 @@
 import re, requests, json
 import pandas as pd 
 import progressbar
-
+from sklearn import preprocessing
+import tensorflow.contrib.learn as skflow
+from sklearn import metrics, cross_validation
 
 class Parser(object):
   def __init__(self, telize_url='http://localhost/geoip/'):
@@ -10,8 +12,9 @@ class Parser(object):
     self.features = ["ip", "src_port", "dst_port", "city", "organization", "continent_code",
               "country", "region", "area_code", "longitude", "country_code3", "region_code", "dma_code", "country_code", "offset",
               "latitude"]
-    self.data = []
-  
+    self.X = [] # inputs
+    self.Y = [] # desired
+
   def run(self, filename, is_ddos, parser):
     """ Run normalization
     :param filename:str -> File to parse.
@@ -30,20 +33,23 @@ class Parser(object):
         elif parser == 'tcpdump':
           src_ip_tcp, src_port, dst_port = re.findall(r'IP (.+?\..+?\..+?\..+?)\.(.+?) > .+?\..+?\..+?\..+?\.(.+?):', raw[i])[0]
         else:
-          print 'heuuu'
           raise Exception('Parser not supported: %s ' % parser)
 
         entry = self.get_ip_geo_info(src_ip_tcp)
         entry['src_port'] = int(src_port)
         entry['dst_port'] = int(dst_port)
-        entry['is_ddos'] = is_ddos
-        self.data.append(entry)
+        self.Y.append(is_ddos)
+        self.X.append(entry)
       except Exception, e:
         print e
         pass# skipped. Could not parse.x
       bar.update(i)
     
     bar.finish()
+
+    # preprocessing
+    self.pd_X = pd.read_json(json.dumps(self.X))
+    self.X_normalized = preprocessing.normalize(self.pd_X, norm='l2')
   
   def get_ip_geo_info(self, ip):
     r = requests.get(self.telize_url + ip)
@@ -57,24 +63,39 @@ class Parser(object):
     return d
 
   def save_data(self, filename):
-    with open(filename, 'w') as f:
-      json.dump(self.data[0], f)
+    pass
+#    with open(filename+'_inputs, 'w') as f:
+#      json.dump(self.X_normalized, f)
+#    with open(filename+'_y, 'w') as f:
+#      json.dump(self.Y, f)
 
-  def print_data(self):
-    print json.dumps(self.data, indent=2)  
+  def print_json(self):
+    print json.dumps(self.X, indent=2)
+
 if __name__ == "__main__":
   import os, re
-  path = './cainelli/mackenzie/tensorflow-ddos/dataset/load/'
+  path = '/Users/cainelli/Documents/workspace/cainelli/mackenzie/tensorflow-ddos/dataset/load/'
   loads = os.listdir(path)
   p = Parser()
+  # Load Data 
   for file in loads:
     parser, is_ddos = re.findall(r'(iptables|tcpdump).+?cnli', file), re.findall(r'(ddos|legit).+?cnli', file)
     if parser and is_ddos:
-      is_ddos = 1 if is_ddos == 'ddos' else 0 
+      is_ddos = 1 if is_ddos[0] == 'ddos' else 0
       p.run(
         filename=path+file,
         parser=parser[0],
         is_ddos=is_ddos
       )
 
-  p.print_data()
+  # Learn Network      
+  def categorical_model(X, y):
+    return skflow.models.logistic_regression(X, y)
+    
+  X_train, X_test, y_train, y_test = cross_validation.train_test_split(p.X_normalized, p.Y, test_size=0.2, random_state=42)
+  classifier = skflow.TensorFlowEstimator(model_fn=categorical_model,
+    n_classes=16, learning_rate=0.07)
+  classifier.fit(X_train, y_train)
+  
+  print("Accuracy: {0}".format(metrics.accuracy_score(classifier.predict(X_test), y_test)))
+
